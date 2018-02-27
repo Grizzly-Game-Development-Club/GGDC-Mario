35b48e86f90b46762e1885276c2b27c82f9b718a
Jesse Merida <36455659+Aperture98@users.noreply.github.com>
2018-02-27 14:36:41 -0500

    Made Controller Script

---
+++ Mario-Project/Assets/Script/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 5f;
+    public float height = 5;
+
+    public bool grounded = false;
+
+    private Rigidbody2D rb2d;
+
+    void Start()
+    {
+        // Sets Rigidbody2D component to rb2d
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.RightArrow) || (Input.GetKey(KeyCode.D)))
+        {
+            transform.Translate(Vector3.right * speed * Time.fixedDeltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow) || (Input.GetKey(KeyCode.A)))
+        {
+            transform.Translate(Vector3.left * speed * Time.fixedDeltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.UpArrow) || (Input.GetKey(KeyCode.W)))
+        {
+            if (grounded == true)
+                rb2d.velocity = new Vector2(0f, height);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        grounded = true;
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        grounded = false;
+    }
+}
